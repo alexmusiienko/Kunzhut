b221478fd98d056d962f231bdeee8b3e05336bb5
snpolishchuk <polischuk.olexandr@gmail.com>
2017-03-11 13:04:52 +0200

    Removed unnecessary variable inside onCreateView method of ChatFragment

--- app/src/main/java/com/example/alexm/kunzhutproject/ChatFragmant.java
+++ app/src/main/java/com/example/alexm/kunzhutproject/ChatFragmant.java
@@ -6,18 +6,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * Created by lubcik on 09.03.17.
- */
-
 public class ChatFragmant extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.chat_fragment, container, false);
-
-        return view;
+        return inflater.inflate(R.layout.chat_fragment, container, false);
     }
 }
