e9aca741f41358730b088de8ed3ba85cac3c8698
Vergel878 <lubcik@MBP-Lubcik.matic.engineering.router>
2017-03-11 13:31:52 +0200

    Did some changes in gitignore file

--- app/src/main/java/com/example/alexm/kunzhutproject/ChatFragmant.java
+++ app/src/main/java/com/example/alexm/kunzhutproject/ChatFragmant.java
@@ -5,13 +5,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class ChatFragmant extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
-
         return inflater.inflate(R.layout.chat_fragment, container, false);
+
+
     }
 }
