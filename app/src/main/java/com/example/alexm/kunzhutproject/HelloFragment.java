35703ee0c931c0aa40ac1379f3cce6a1c61a3793
snpolishchuk <polischuk.olexandr@gmail.com>
2017-03-11 13:09:57 +0200

    Removed unused variable

--- app/src/main/java/com/example/alexm/kunzhutproject/HelloFragment.java
+++ app/src/main/java/com/example/alexm/kunzhutproject/HelloFragment.java
@@ -11,7 +11,6 @@ import android.widget.EditText;
 
 public class HelloFragment extends Fragment {
     private Button LetsStarButtom;
-    private EditText Greetings;
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
