b0fae0d168a1748f7cb85171add36f30da014ed5
Vergel878 <lubcik@MBP-Lubcik.matic.engineering.router>
2017-03-11 12:22:40 +0200

    Did better view of Chat Layout

--- app/src/main/java/com/example/alexm/kunzhutproject/ChatFragmant.java
+++ app/src/main/java/com/example/alexm/kunzhutproject/ChatFragmant.java
@@ -1,10 +1,23 @@
 package com.example.alexm.kunzhutproject;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by lubcik on 09.03.17.
  */
 
 public class ChatFragmant extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.chat_fragment, container, false);
+
+        return view;
+    }
 }
