41d52312eee2a228b50460dc23150ef70d501264
Vergel878 <lubcik@MacBook-Pro-Lubcik.local>
2017-03-15 22:53:15 +0200

    Adding Help Activity with not correct text about visa

---
+++ app/src/main/java/com/example/alexm/kunzhutproject/HelpFragment.java
@@ -0,0 +1,23 @@
+package com.example.alexm.kunzhutproject;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by lubcik on 15.03.17.
+ */
+
+public class HelpFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.help_fragment, container, false);
+
+}
+}
