55be22c09ae327d6b074afbd2223d94781b5552b
yanaddn <Yana.Didun.KN.2016@lpnu.ua>
2017-03-25 16:07:30 +0200

    Added onViewCreate, showFragment, goToPreviousFragment

--- app/src/main/java/com/example/alexm/kunzhutproject/MainActivity.java
+++ app/src/main/java/com/example/alexm/kunzhutproject/MainActivity.java
@@ -1,28 +1,50 @@
 package com.example.alexm.kunzhutproject;
 
 import android.os.Bundle;
-
-
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
+import static android.app.PendingIntent.getActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);}
 
+    protected void onViewCreate(){
         setContentView(R.layout.activity_main);
-        showFragment(new HelloFragment());
+        replaceFragment(new HelloFragment());
     }
 
-    public void showFragment(Fragment fragment) {
+    public void replaceFragment(Fragment fragment) {
+
         FragmentManager supportFragmentManager = getSupportFragmentManager();
+
         FragmentTransaction fragmentTransaction = supportFragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.container,fragment);
+        fragmentTransaction.replace(R.id.container, fragment);
+
         fragmentTransaction.commit();
     }
+
+    //LAST TASKS
+    public void showFragment(Fragment fragment) {
+
+        FragmentActivity activity = getActivity();
+        MainActivity mainActivity = (MainActivity) activity;
+
+        mainActivity.showFragment(new ExampleFragment());
+    }
+
+    public void goToPreviousFragment(Fragment fragment) {
+
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+        FragmentManager fm = getActivity().supportFragmentManager();
+
+        fm.popBackStack();
+    }
+}
 }
