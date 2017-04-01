4e1958d8c7fb0864c53f2a32eeeae989792ddfdd
Daria Savinkina <dsavinkina@gmail.com>
2017-04-01 14:02:55 +0300

    - Added toolbar to HelpFragment and SelectionFragment. - Fixed issue with incorrect argument list for showFragment method

--- app/src/main/java/com/example/alexm/kunzhutproject/SelectionFragment.java
+++ app/src/main/java/com/example/alexm/kunzhutproject/SelectionFragment.java
@@ -1,37 +1,20 @@
 package com.example.alexm.kunzhutproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.ViewGroup;
 
-public class SelectionFragment extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+/**
+ * Created by polis on 24.03.2017.
+ */
 
-    Spinner spinner;
+public class SelectionFragment extends Fragment {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.selection_screen);
-
-        spinner = (Spinner) findViewById(R.id.spinner);
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.visa, android.R.layout.simple_spinner_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        TextView myText = (TextView) view;
-        Toast.makeText(this, "You Selected "+myText.getText(), Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.selection_screen, container, false);
     }
 }
-
