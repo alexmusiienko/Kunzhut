0e4cd05e9c3d717892eff7a2b838c3379f57dbe5
yanaddn <Yana.Didun.KN.2016@lpnu.ua>
2017-04-09 21:21:10 +0300

    Created new class BooleanQuestion.java

---
+++ app/src/main/java/com/example/alexm/kunzhutproject/BooleanQuestion.java
@@ -0,0 +1,14 @@
+package com.example.alexm.kunzhutproject;
+
+public class BooleanQuestion {
+
+    private boolean answer;
+
+    public void setAnswer(boolean answer){
+        this.answer = answer;
+    }
+
+    public BooleanQuestion(String s) {
+        super(questionTitle);
+    }
+}
