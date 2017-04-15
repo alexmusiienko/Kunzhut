c4e4fe586927a036b1b205f0c0e39c4f62a1e4d0
yanaddn <Yana.Didun.KN.2016@lpnu.ua>
2017-04-15 13:41:24 +0300

    Added ChatAdapter in ChatFragmant.java

--- app/src/main/java/com/example/alexm/kunzhutproject/ChatFragmant.java
+++ app/src/main/java/com/example/alexm/kunzhutproject/ChatFragmant.java
@@ -1,25 +1,49 @@
 package com.example.alexm.kunzhutproject;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.Toolbar;
+import android.widget.Adapter;
 
 public class ChatFragmant extends Fragment {
+
+    RecyclerView recyclerView;
+    Adapter adapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
-
-        final View view = getView();
-
         return inflater.inflate(R.layout.chat_fragment, container, false);
-
     }
 
+    @Override
+    public void onViewCreated(final View view, @Nullable final Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+        recyclerView = (RecyclerView) getView().findViewById(R.id.example);
+
+        recyclerView.setAdapter((RecyclerView.Adapter) adapter);
+    }
+
+    class ChatAdapter extends RecyclerView.Adapter {
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+    }
 }
