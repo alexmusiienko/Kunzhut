26a085a8aee3e786b3334e57df40aebaf469bf7c
lubcik <LDrevych@gmail.com>
2017-04-30 22:13:40 +0300

    Add ChatAdapter  with 2 list 1 of this is list of answer

--- app/src/main/java/com/example/alexm/kunzhutproject/ChatFragmant.java
+++ app/src/main/java/com/example/alexm/kunzhutproject/ChatFragmant.java
@@ -109,4 +109,67 @@ public class ChatFragment extends Fragment {
             thirdOptionButton.setOnClickListener(onClickListener);
         }
     }
+    private static class ChatAdapter extends RecyclerView.Adapter<QuestionViewHolder> {
+
+        private static final int BOOLEAN_QUESTION_VIEW = 100;
+        private static final int MULTIPLE_OPTIONS_QUESTION_VIEW = 200;
+
+        private RecyclerView recyclerView;
+
+        private List<Question> questions;
+        private List<Question> displayedQuestions;
+
+        private ChatAdapter(final RecyclerView recyclerView) {
+            this.recyclerView = recyclerView;
+        }
+
+        public void setQuestions(final List<Question> questions) {
+            this.questions = questions;
+            this.displayedQuestions = new ArrayList<>();
+
+            displayedQuestions.add(questions.remove(0));
+
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getItemViewType(final int position) {
+            final Question question = displayedQuestions.get(position);
+
+            if (question instanceof BooleanQuestion) {
+                return BOOLEAN_QUESTION_VIEW;
+            }
+
+            return MULTIPLE_OPTIONS_QUESTION_VIEW;
+        }
+
+        @Override
+        public QuestionViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
+            int layoutId;
+
+            if (viewType == BOOLEAN_QUESTION_VIEW) {
+                layoutId = R.layout.yes_no_bubbles;
+            } else {
+                layoutId = R.layout.select_mes_bot;
+            }
+
+            final View view = LayoutInflater.from(parent.getContext()).inflate(layoutId, parent, false);
+
+            if (viewType == BOOLEAN_QUESTION_VIEW) {
+                return new BooleanQuestionViewHolder(view);
+            } else {
+                return new MultipleOptionsQuestionViewHolder(view);
+            }
+        }
+
+        @Override
+        public void onBindViewHolder(final QuestionViewHolder holder, final int position) {
+            final Question question = displayedQuestions.get(position);
+
+            if (question instanceof BooleanQuestion) {
+                setupBooleanHolder((BooleanQuestionViewHolder) holder, question);
+            } else {
+                setupMultipleOptionsHolder((MultipleOptionsQuestionViewHolder) holder, question);
+            }
+        }
 
