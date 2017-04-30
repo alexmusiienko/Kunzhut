d8b081fa2281967ac2aaaa505939d677a5db0bd4
yanaddn <Yana.Didun.KN.2016@lpnu.ua>
2017-04-30 21:47:04 +0300

    Update QuestionFactory
    Fixed getters

--- app/src/main/java/com/example/alexm/kunzhutproject/QuestionFactory.java
+++ app/src/main/java/com/example/alexm/kunzhutproject/QuestionFactory.java
@@ -5,69 +5,75 @@ import java.util.List;
 
 public class QuestionFactory {
 
-    public static List<Question> qetStudentVisa() {
+    public static List<Question> getStudentVisaQuestions() {
         final List<Question> questions = new ArrayList<>();
 
-        questions.add(new Question("Вам є повних 18 р.?"));
-        questions.add(new Question("Ви вже подорожували до Америки?"));
-        questions.add(new Question("У вас є медичне страхування?"));
-        questions.add(new Question("Ви маєте квитки туди і назад?"));
-        questions.add(new Question("Ви маєте міжнародний студентський квиток?"));
+        questions.add(new BooleanQuestion("Вам є повних 18 р.?"));
+
+        List<String> options = new ArrayList<>();
+        options.add("1");
+        options.add("2");
+        options.add("3");
+
+        questions.add(new MultipleOptionQuestion("1, 2, 3?", options));
+        questions.add(new BooleanQuestion("Ви вже подорожували до Америки?"));
+        questions.add(new BooleanQuestion("У вас є медичне страхування?"));
+        questions.add(new BooleanQuestion("Ви маєте квитки туди і назад?"));
+        questions.add(new BooleanQuestion("Ви маєте міжнародний студентський квиток?"));
 
         return questions;
     }
 
-    public static List<Question> qetWorkingVisa() {
+    public static List<Question> getWorkingVisaQuestions() {
         final List<Question> questions = new ArrayList<>();
 
-        questions.add(new Question("Вам є повних 18 р.?"));
-        questions.add(new Question("Ви вже подорожували до Америки?"));
-        questions.add(new Question("У вас є медичне страхування?"));
-        questions.add(new Question("Ви маєте квитки туди і назад?"));
-        questions.add(new Question("Ви подбали про місце ночівлі?"));
-        questions.add(new Question("У вас є діти?"));
+        questions.add(new BooleanQuestion("Вам є повних 18 р.?"));
+        questions.add(new BooleanQuestion("Ви вже подорожували до Америки?"));
+        questions.add(new BooleanQuestion("У вас є медичне страхування?"));
+        questions.add(new BooleanQuestion("Ви маєте квитки туди і назад?"));
+        questions.add(new BooleanQuestion("Ви подбали про місце ночівлі?"));
+        questions.add(new BooleanQuestion("У вас є діти?"));
 
         return questions;
     }
 
-    public static List<Question> qetBusinessVisa() {
+    public static List<Question> getBusinessVisaQuestions() {
         final List<Question> questions = new ArrayList<>();
 
-        questions.add(new Question("Вам є повних 18 р.?"));
-        questions.add(new Question("Ви вже подорожували до Америки?"));
-        questions.add(new Question("У вас є медичне страхування?"));
-        questions.add(new Question("Ви маєте квитки туди і назад?"));
-        questions.add(new Question("Ви подбали про місце ночівлі?"));
-        questions.add(new Question("У вас є діти?"));
+        questions.add(new BooleanQuestion("Вам є повних 18 р.?"));
+        questions.add(new BooleanQuestion("Ви вже подорожували до Америки?"));
+        questions.add(new BooleanQuestion("У вас є медичне страхування?"));
+        questions.add(new BooleanQuestion("Ви маєте квитки туди і назад?"));
+        questions.add(new BooleanQuestion("Ви подбали про місце ночівлі?"));
+        questions.add(new BooleanQuestion("У вас є діти?"));
 
         return questions;
     }
 
-    public static List<Question> qetExursionVisa() {
+    public static List<Question> getExursionVisaQuestions() {
         final List<Question> questions = new ArrayList<>();
 
-        questions.add(new Question("Вам є повних 18 р.?"));
-        questions.add(new Question("Ви вже подорожували до Америки?"));
-        questions.add(new Question("У вас є медичне страхування?"));
-        questions.add(new Question("Ви маєте квитки туди і назад?"));
-        questions.add(new Question("Ви подбали про місце ночівлі?"));
-        questions.add(new Question("У вас є діти?"));
+        questions.add(new BooleanQuestion("Вам є повних 18 р.?"));
+        questions.add(new BooleanQuestion("Ви вже подорожували до Америки?"));
+        questions.add(new BooleanQuestion("У вас є медичне страхування?"));
+        questions.add(new BooleanQuestion("Ви маєте квитки туди і назад?"));
+        questions.add(new BooleanQuestion("Ви подбали про місце ночівлі?"));
+        questions.add(new BooleanQuestion("У вас є діти?"));
 
         return questions;
     }
 
-    public static List<Question> qetShoppingVisa() {
+    public static List<Question> getShoppingVisaQuestions() {
         final List<Question> questions = new ArrayList<>();
 
-        questions.add(new Question("Вам є повних 18 р.?"));
-        questions.add(new Question("Ви вже подорожували до Америки?"));
-        questions.add(new Question("У вас є медичне страхування?"));
-        questions.add(new Question("Ви маєте квитки туди і назад?"));
-        questions.add(new Question("Ви подбали про місце ночівлі?"));
-        questions.add(new Question("У вас є діти?"));
-        questions.add(new Question("Ви вже обрали торговий центр?"));
+        questions.add(new BooleanQuestion("Вам є повних 18 р.?"));
+        questions.add(new BooleanQuestion("Ви вже подорожували до Америки?"));
+        questions.add(new BooleanQuestion("У вас є медичне страхування?"));
+        questions.add(new BooleanQuestion("Ви маєте квитки туди і назад?"));
+        questions.add(new BooleanQuestion("Ви подбали про місце ночівлі?"));
+        questions.add(new BooleanQuestion("У вас є діти?"));
+        questions.add(new BooleanQuestion("Ви вже обрали торговий центр?"));
 
         return questions;
     }
 }
-
