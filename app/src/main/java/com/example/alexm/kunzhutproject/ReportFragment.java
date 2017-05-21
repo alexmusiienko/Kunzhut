37c10b31644e8ef8afd5ddd733c276c4e23821f8
lubcik <LDrevych@gmail.com>
2017-05-21 10:45:48 +0300

    Added adapter  with steps_layout in which will draw our text

--- app/src/main/java/com/example/alexm/kunzhutproject/ReportFragment.java
+++ app/src/main/java/com/example/alexm/kunzhutproject/ReportFragment.java
@@ -1,27 +1,24 @@
 package com.example.alexm.kunzhutproject;
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+        import android.os.Bundle;
+        import android.support.annotation.Nullable;
+        import android.support.v4.app.Fragment;
+        import android.support.v7.widget.LinearLayoutManager;
+        import android.support.v7.widget.RecyclerView;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.Adapter;
+        import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
+        import java.util.ArrayList;
+        import java.util.List;
 
-public class  ReportFragment extends Fragment {
-    private List<Question> displayedQuestions;
-    private RecyclerView recyclerView2;
-    private List<Question> questions;
+public class ReportFragment extends Fragment {
 
-    public void setQuestions(final List<Question> questions) {
-        this.questions = questions;
-    }
-
-    public void setListOfAnwer(List<Question> displayedQuestions) {
-        this.displayedQuestions = displayedQuestions;
+    private List<Question> answers;
+    public void setAnswers(final List<Question> questions) {
+        this.answers = answers;
     }
 
     @Override
@@ -31,55 +28,57 @@ public class  ReportFragment extends Fragment {
         return inflater.inflate(R.layout.report_fragment, container, false);
     }
 
-   /*@Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        ArrayList<String> steps = new ArrayList();
+        steps.add("«Импортонезависимую технологическую платформу для разработки распределенных информационно-вычислительных систем на основе свободного" +
+                " программного обеспечения» разработали ученые лаборатории информационных " +
+                "технологий Всероссийского НИИ овцеводства");
+        steps.add("«На сегодняшний день существуют российские программные " +
+                "продукты, но почти все они работают под управлением операционной системы Windows," +
+                " либо используют программные библиотеки и компоненты иностранных " +
+                "компаний, а значит в любом случае импортозависимы");
         super.onViewCreated(view, savedInstanceState);
-        recyclerView2 = (RecyclerView) getView().findViewById(R.id.recyclerView1);
-        recyclerView2.setQuestions(questions);
-        recyclerView2.setListOfAnswer(displayedQuestions);
-    }*/
+        final RecyclerView recyclerView = (RecyclerView) getView().findViewById(R.id.recyclerView1);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        ReportAdapter adapter = new ReportAdapter ();
+        adapter.setSteps(steps);
+        recyclerView.setAdapter(adapter);
+    }
 
-    private static class ReportAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private static class ReportAdapter extends RecyclerView.Adapter<StepViewHolder>{
+        private List<String> steps;
 
-        private RecyclerView recyclerView;
-        private List<Question> questions;
-
-        private ReportAdapter(final RecyclerView recyclerView) {
-            this.recyclerView = recyclerView;
-        }
-
-        public void setQuestions(final List<Question> questions) {
-            this.questions = questions;
-            this.questions = new ArrayList<>();
-
-            questions.add(questions.remove(0));
-
+        public void setSteps(List<String> steps) {
+            this.steps = steps;
             notifyDataSetChanged();
         }
 
         @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return null;
+        public StepViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            int layoutForSteps = R.id.steps_layout;
+            final View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.steps_layout, parent, false);
+            return new StepViewHolder(view);
         }
 
         @Override
-        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
-           /*
-            recyclerViewReport = (RecyclerView) getView().findViewById(R.id.recyclerView);
-            recyclerViewReport.setLayoutManager(new LinearLayoutManager(getContext()));
-            recyclerViewReport.setAdapter((RecyclerView.Adapter) ReportAdapter)
-            ReportAdapter.setQuestions(questions);
-
-            final Question question = displayedQuestions.get(position);
-           */
+        public void onBindViewHolder(StepViewHolder holder, int position) {
+            final String stepText = steps.get(position);
+            holder.stepView.setText(steps.get(position));
         }
 
         @Override
         public int getItemCount() {
-            return questions.size();
+            return steps.size();
         }
     }
-}
+    private static class StepViewHolder extends RecyclerView.ViewHolder {
 
+        private TextView stepView;
 
+        public StepViewHolder(final View itemView) {
+            super(itemView);
+            stepView = (TextView) itemView.findViewById(R.id.stepView);
+        }
+
+    }}
